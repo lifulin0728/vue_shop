6ce7a495968c3592b90ed0bc1e7cc0c9cdb19f80
lemon <5646396+lemon28@user.noreply.gitee.com>
2020-03-17 08:23:21 +0800

    完成了报表功能的开发

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,7 @@ import GoodsList from '../components/goods/List.vue'
 import Add from '../components/goods/Add.vue'
 import Edit from '../components/goods/Edit.vue'
 import Order from '../components/order/Order.vue'
+import Report from '../components/report/Report.vue'
 
 Vue.use(VueRouter)
 
@@ -68,6 +69,10 @@ const routes = [
 			{
 				path: '/orders',
 				component: Order
+			},
+			{
+				path: '/reports',
+				component: Report
 			}
 		]
 	}
